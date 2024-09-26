b882325e5d34b6638ba09b5098d0d1c32f22e6d8
JuniorSidnei <sidneicarrarojunior@gmail.com>
2024-09-26 16:41:00 -0300

    initial commit

---
+++ Source/Slash/Private/Items.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Items.h"
+
+AItems::AItems() {
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AItems::BeginPlay() {
+	Super::BeginPlay();
+}
+
+void AItems::Tick(float DeltaTime) {
+	Super::Tick(DeltaTime);
+}
