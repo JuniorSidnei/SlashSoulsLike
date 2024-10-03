7e39815203e52c223811e86e8d345e687cab8c0a
JuniorSidnei <sidneicarrarojunior@gmail.com>
2024-10-03 20:46:36 -0300

    bp item functions

--- Source/Slash/Private/Items.cpp
+++ Source/Slash/Private/Items.cpp
@@ -1,13 +1,43 @@
-#include "Items.h"
+#include <Items.h>
+#include <Components/SphereComponent.h>
 
 AItems::AItems() {
 	PrimaryActorTick.bCanEverTick = true;
+
+	m_mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
+	RootComponent = m_mesh;
+
+	m_sphereCollision = CreateDefaultSubobject<USphereComponent>(TEXT("SphereCollision"));
+	m_sphereCollision->SetupAttachment(GetRootComponent());
 }
 
 void AItems::BeginPlay() {
 	Super::BeginPlay();
+
+	m_sphereCollision->OnComponentBeginOverlap.AddDynamic(this, &AItems::OnShpereStartOverlap);
+	m_sphereCollision->OnComponentEndOverlap.AddDynamic(this, &AItems::OnShpereEndOverlap);
+}
+
+float AItems::TransformedSin() const {
+	return Amplitude * FMath::Sin(m_runningTime * TimeConstant);
+}
+
+float AItems::TransformedCos() const {
+	return Amplitude * FMath::Cos(m_runningTime * TimeConstant);
+}
+
+void AItems::OnShpereStartOverlap(UPrimitiveComponent* overlappedComponent, AActor* otherActor,
+	UPrimitiveComponent* otherComp, int32 otherBodyIndex, bool bFromSweep, const FHitResult& sweepResult) {
+	
+}
+
+void AItems::OnShpereEndOverlap(UPrimitiveComponent* overlappedComponent, AActor* otherActor,
+	UPrimitiveComponent* otherComp, int32 otherBodyIndex) {
+	
 }
 
 void AItems::Tick(float DeltaTime) {
 	Super::Tick(DeltaTime);
+
+	m_runningTime += DeltaTime;
 }
--- Source/Slash/Public/Items.h
+++ Source/Slash/Public/Items.h
@@ -4,14 +4,42 @@
 #include "GameFramework/Actor.h"
 #include "Items.generated.h"
 
+class USphereComponent;
+
 UCLASS()
 class SLASH_API AItems : public AActor {
 	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere)
+	float Amplitude;
+
+	UPROPERTY(EditAnywhere)
+	float TimeConstant;
 	
-public:	
 	AItems();
 	virtual void Tick(float DeltaTime) override;
-	
-protected:
 	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintPure)
+	float TransformedSin() const;
+
+	UFUNCTION(BlueprintPure)
+	float TransformedCos() const;
+
+	UFUNCTION()
+	void OnShpereStartOverlap(UPrimitiveComponent* overlappedComponent, AActor* otherActor, UPrimitiveComponent* otherComp, int32 otherBodyIndex, bool bFromSweep, const FHitResult& sweepResult);
+
+	UFUNCTION()
+	void OnShpereEndOverlap(UPrimitiveComponent* overlappedComponent, AActor* otherActor, UPrimitiveComponent* otherComp, int32 otherBodyIndex);
+	
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	float m_runningTime;
+	
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* m_mesh;
+
+	UPROPERTY(VisibleAnywhere)
+	USphereComponent* m_sphereCollision;
 };
